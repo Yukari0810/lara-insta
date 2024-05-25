723986ffbb3539d1c0bfd0385ab6d57092ead68a
Yukari0810 <147824424+Yukari0810@users.noreply.github.com>
2024-05-25 23:03:42 +0900

    Update Like.php

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -15,6 +15,6 @@ class Like extends Model
         return $this->belongsTo(Post::class);
     }
     public function user(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 }
